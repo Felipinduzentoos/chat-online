20c71c62165dc1c33903d688a44fc0adc81b7453
Felipinduzentoos <felipin200dedano@gmail.com>
2024-09-16 20:50:25 -0300

    a

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const http = require('http').Server(app);
-const serverSocket = require('socket.io')(http);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+
+
 
 http.listen(3000, () => {
-  console.log('iniciando na porta 3000');
+  console.log('Servidor iniciado na porta 3000');
 });
 
-serverSocket.on('connection', function(socket) {
-    console.log('algo se conectou' + socket.id);
+
+io.on('connection', (socket) => {
+    console.log('Cliente conectado: ' + socket.id);
+
     
-    socket.on('msg', function(mensagem){
-      console.log (`msg recebida ${mensagem}`)
-      serverSocket.emit('msg', ` ${mensagem}`)
-    })
-    })
+    socket.on('msg', (data) => {
+        console.log(`Mensagem recebida: ${data.username}: ${data.message}`);
+        io.emit('msg', data); 
+    });
+
+ 
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado: ' + socket.id);
+    });
+});
